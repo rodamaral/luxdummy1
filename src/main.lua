b00a794e0c152b284c27da882bfd7cad7c6ff086
Rodrigo A. do Amaral <rodrigo.zfc@gmail.com>
2025-12-22 14:47:40 -0300

    chore: add more deps

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,10 @@
-print("Hello world!")
+local argparse = require("argparse")
+local inspect = require("inspect")
+local cjson = require("cjson")
+
+return {
+    _VERSION = "0.1.0",
+    argparse = argparse,
+    inspect = inspect,
+    cjson = cjson,
+}
