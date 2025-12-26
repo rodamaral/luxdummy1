608d7217e2e87124f7fbd216fb17a8908f4d86c2
Rodrigo A. do Amaral <rodrigo.zfc@gmail.com>
2025-12-25 23:17:12 -0300

    feat: print versions in main.lua

--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,10 @@ local argparse = require("argparse")
 local inspect = require("inspect")
 local cjson = require("cjson")
 
+print('luxdummy1', cjson._VERSION, inspect._VERSION, 'should be 2.1.0 & 3.1.3')
+
 return {
+    name = 'luxdummy1',
     _VERSION = "0.1.0",
     argparse = argparse,
     inspect = inspect,
